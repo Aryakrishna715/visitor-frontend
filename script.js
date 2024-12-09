ec1a7121ce217c8a8098e116dba3bdae61f604d2
Aryakrishna715 <aryakrishna715@gmail.com>
2024-12-09 21:10:36 +0530

    script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ async function submitForm(event) {
 
     try {
         // Make a POST request to submit the form data to the backend
-        const response = await fetch('https://visitor-backend-7.onrender.com/submit', { // Ensure the backend route matches
+        const response = await fetch('https://visitor-backend-8.onrender.com/submit', { // Ensure the backend route matches
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
